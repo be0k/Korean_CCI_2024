ff8bd8e18ff8f98d5e57c9ded209ead817c4f4d7
be0k <befinetuning@gmail.com>
2024-08-26 20:31:13 +0900

    add shell

---
+++ test.sh
@@ -0,0 +1,33 @@
+# 리스트를 반복하여 사용
+for i in {0..10}
+do
+    if [ $i -eq 10 ]; then
+        param=1890
+    else
+        param=2040
+    fi
+   python src/train.py\
+   --output output/fold$i.json\
+    --model_id kihoonlee/STOCK_SOLAR-10.7B\
+    --tokenizer kihoonlee/STOCK_SOLAR-10.7B\
+    --device cuda\
+    --device_number 1\
+    --peft_model_dir ./test_git/checkpoint-$param\
+    --test_dir data/test.json
+done
+
+
+python src/ensemble.py\
+--dir output\
+--d0 fold0.json\
+--d1 fold1.json\
+--d2 fold2.json\
+--d3 fold3.json\
+--d4 fold4.json\
+--d5 fold5.json\
+--d6 fold6.json\
+--d7 fold7.json\
+--d8 fold8.json\
+--d9 fold9.json\
+--d10 fold10.json\
+--save_dir ensemble.json
