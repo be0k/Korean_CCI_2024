fddc4086b909c0504e6b5166628488e7e7522410
Junyoung Kim <befinetuning@gmail.com>
2024-08-27 23:29:32 +0900

    Update test.py

--- run/test.py
+++ run/test.py
@@ -24,17 +24,19 @@ import os
 parser = argparse.ArgumentParser(prog="test", description="Testing about Conversational Context Inference.")
 
 g = parser.add_argument_group("Common Parameter")
-g.add_argument("--output",type=str, default=f'quan8_auto.json',  help="output filename")
+g.add_argument("--output",type=str, default=f'output/fold0.json',  help="output filename")
 g.add_argument("--model_id", type=str, default='kihoonlee/STOCK_SOLAR-10.7B', help="huggingface model id")
 g.add_argument("--tokenizer", type=str, help="huggingface tokenizer")
 g.add_argument("--device", type=str, default='cuda', help="device to load the model")
-g.add_argument("--peft_model_dir", type=str, default="./test_git/checkpoint-614",help="peft_model_dir")
+g.add_argument("--peft_model_dir", type=str, default="./output/fold0/checkpoint-2040",help="peft_model_dir")
 g.add_argument("--test_dir", type=str, default="data/test.json", help="test dir path")
 
 # fmt: on
 
 
 def main(args):
+    print(args)
+    
     #model
     bnb_config = BitsAndBytesConfig(
         load_in_8bit=True,
@@ -99,14 +101,14 @@ def main(args):
     answer = answer.reshape(6, len(answer)//6)
     for i, custom_dict in enumerate(list(itertools.permutations([0,1,2]))):
         custom_dict = {value: index for index, value in enumerate(custom_dict)}
-        print(custom_dict)
+        #print(custom_dict)
         answer[i] =  np.array([custom_dict[value] for value in answer[i].tolist()])
         
     from scipy import stats
     mode_values = stats.mode(answer, axis=0).mode
     for idx, label in enumerate(mode_values):
         result[idx]["output"] = answer_dict[label]
-        print(answer_dict[label])
+        #print(answer_dict[label])
     with open(args.output, "w", encoding="utf-8") as f:
         f.write(json.dumps(result, ensure_ascii=False, indent=4))
 
